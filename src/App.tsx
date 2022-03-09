d1f729d37b6f126aac21f3c937b594f43acb4842
Suresh <sureshnani86@gmail.com>
2022-03-09 15:26:07 +0000

    Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import MuiButton from "./components/MuiButton";
 import { MuiTypography } from "./components/MuiTypography";
 
 
 function App() {
   return <div className="App">
-    <MuiTypography></MuiTypography>
+    {/* <MuiTypography></MuiTypography> */}
+    <MuiButton></MuiButton>
     </div>;
 }
 
